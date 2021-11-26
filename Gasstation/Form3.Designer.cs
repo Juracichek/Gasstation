ae8fb30b29c7bd1e994d2bdfbd9892b17043631a
Juracichek <jury.a.ma@gmail.com>
2021-11-26 17:37:47 +0300

    new

--- Gasstation/Form3.Designer.cs
+++ Gasstation/Form3.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            this.ClientSize = new System.Drawing.Size(1013, 1050);
+            this.Name = "Form3";
+            this.Text = "Interface";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
